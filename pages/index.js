0365a33ff45bb4c27af3becc51b676bb3e6b6d9e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2024-09-03 10:57:17 +0700

    privacy policy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,6 +11,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Tab bar at the top */}
+      <nav className="tab-bar">
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+          </li>
+        </ul>
+      </nav>
+
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
@@ -18,6 +31,31 @@ export default function Home() {
       </main>
 
       <Footer />
+
+      {/* Styling for the tab bar */}
+      <style jsx>{`
+        .tab-bar {
+          background-color: #f8f9fa;
+          padding: 10px;
+          border-bottom: 1px solid #e7e7e7;
+          text-align: center;
+        }
+        .tab-bar ul {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+          display: flex;
+          justify-content: center;
+        }
+        .tab-bar li {
+          margin: 0 15px;
+        }
+        .tab-bar a {
+          text-decoration: none;
+          color: #0070f3;
+          font-weight: bold;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
